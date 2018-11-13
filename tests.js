ade8905c9de64be8fbae51134f14963eaddba02a
Bailey D'Amour <bailey.damour@cloud.statcan.ca>
2018-11-13 12:00:14 -0500

    more descriptive tests for evaluate

--- tests.js
+++ tests.js
@@ -40,62 +40,64 @@ describe('VectorLib', function() {
     });
 
     describe('#evaluate', function() {
-        it("should handle vector arithmetic", function() {
-            let vectors = {
-                'v1': [
-                    {'refper': "2018-01-01", 'value': 1},
-                    {'refper': "2018-02-01", 'value': 2}
-                ],
-                'v2': [
-                    {'refper': "2018-01-01", 'value': 3},
-                    {'refper': "2018-02-01", 'value': 4}
-                ],
-                'v3': [
-                    {'refper': "2018-01-01", 'value': 2},
-                    {'refper': "2018-02-01", 'value': 2}
-                ],
-                'v4': [
-                    {'refper': "2018-01-01", 'value': 1},
-                    {'refper': "2018-02-01", 'value': 2},
-                    {'refper': "2018-03-01", 'value': 3}
-                ],
-                'v5': [
-                    {'refper': "2018-01-01", 'value': 4},
-                    {'refper': "2018-02-01", 'value': 5},
-                    {'refper': "2018-03-01", 'value': 6}
-                ],
-                'v6': [
-                    {'refper': "2018-01-01", 'value': 7},
-                    {'refper': "2018-02-01", 'value': 8},
-                    {'refper': "2018-03-01", 'value': 9}
-                ]
-            };
-
-            let vexp = "(v1 + v2) * (2*v3)"
-            let expected = [
-                {'refper': "2018-01-01", 'value': 16},
-                {'refper': "2018-02-01", 'value': 24}
-            ];
-            let result = vlib.evaluate(vexp, vectors);
-            assert.strictEqual(vlib.equals(result, expected), true);
-
-            vexp = "(v1 - v2) * (2*v3)";
-            expected = [
-                {'refper': "2018-01-01", 'value': -8},
-                {'refper': "2018-02-01", 'value': -8}
-            ];
-            result = vlib.evaluate(vexp, vectors);
-            assert.strictEqual(vlib.equals(result, expected), true);
-
-            vexp = "v6 - v5 - v4";
-            expected = [
+        let vectors = {
+            'v1': [
+                {'refper': "2018-01-01", 'value': 1},
+                {'refper': "2018-02-01", 'value': 2}
+            ],
+            'v2': [
+                {'refper': "2018-01-01", 'value': 3},
+                {'refper': "2018-02-01", 'value': 4}
+            ],
+            'v3': [
                 {'refper': "2018-01-01", 'value': 2},
-                {'refper': "2018-02-01", 'value': 1},
-                {'refper': "2018-03-01", 'value': 0}
-            ];
-            result = vlib.evaluate(vexp, vectors);
-            assert.strictEqual(vlib.equals(result, expected), true);
-        });
+                {'refper': "2018-02-01", 'value': 2}
+            ],
+            'v4': [
+                {'refper': "2018-01-01", 'value': 1},
+                {'refper': "2018-02-01", 'value': 2},
+                {'refper': "2018-03-01", 'value': 3}
+            ],
+            'v5': [
+                {'refper': "2018-01-01", 'value': 4},
+                {'refper': "2018-02-01", 'value': 5},
+                {'refper': "2018-03-01", 'value': 6}
+            ],
+            'v6': [
+                {'refper': "2018-01-01", 'value': 7},
+                {'refper': "2018-02-01", 'value': 8},
+                {'refper': "2018-03-01", 'value': 9}
+            ]
+        };
+
+        let itVexp = function(vexp, expected) {
+            it (vexp + " should equal " + JSON.stringify(expected), function() {
+                let result = vlib.evaluate(vexp, vectors);
+                assert.strictEqual(vlib.equals(result, expected), true);
+            });
+        };
+
+        let expected = [
+            {'refper': "2018-01-01", 'value': 16},
+            {'refper': "2018-02-01", 'value': 24}
+        ];
+        let vexp = "(v1 + v2) * (2*v3)";
+        itVexp(vexp, expected);
+
+        vexp = "(v1 - v2) * (2*v3)";
+        expected = [
+            {'refper': "2018-01-01", 'value': -8},
+            {'refper': "2018-02-01", 'value': -8}
+        ];
+        itVexp(vexp, expected);
+
+        vexp = "v6 - v5 - v4";
+        expected = [
+            {'refper': "2018-01-01", 'value': 2},
+            {'refper': "2018-02-01", 'value': 1},
+            {'refper': "2018-03-01", 'value': 0}
+        ];       
+        itVexp(vexp, expected);
     });
 
     describe('#intersection', function() {
