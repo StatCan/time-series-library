3a5978e2bada3bf63083e237fa3ee40cadfd5a73
Bailey D'Amour <bailey.damour@cloud.statcan.ca>
2019-07-17 10:09:20 -0400

    Modify configuration for TS compile and rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
-import {main} from './package.json';
-
+import {name} from './package.json';
 
 export default {
-    input: `src/${main}`,
+    input: `module/${name}.js`,
     output: {
-        file: `dist/${main}`,
+        exports: 'named',
+        file: `dist/${name}.js`,
         format: 'iife',
-        name: 'time_series_library'
+        name: 'TimeSeriesLibrary'
     },
     plugins: [
         resolve(),
